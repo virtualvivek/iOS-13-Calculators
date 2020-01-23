3fd0bd40a519b9dd5adeb4fb89e3920e0edfd5b7
Vivek Verma <vivekverma2499@gmail.com>
2020-01-24 00:07:09 +0530

    Added js for index -v 2.4

---
+++ ng-app/viv_preview.js
@@ -0,0 +1,20 @@
+    function Basic(){
+        document.getElementById("calc_title").innerHTML = "Basic Calculator";
+    };
+    function Timer(){
+        document.getElementById("calc_title").innerHTML = "Timer";
+    };
+    function Macro(){
+        document.getElementById("calc_title").innerHTML = "Macro Calculator";
+    };
+    function BMI(){
+        document.getElementById("calc_title").innerHTML = "BMI Calculator";
+    };    
+        
+    function Grade(){
+        document.getElementById("calc_title").innerHTML = "Grade Calculator";
+    };
+        
+    function Default(){
+        document.getElementById("calc_title").innerHTML = "Choose an app";
+    };    
